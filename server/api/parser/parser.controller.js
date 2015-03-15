6ef29f8530204f756b67e4bb0f0aa81e92011814
Eli Sklar <eli.sklar@gmail.com>
2015-03-15 07:57:59 +0200

    Doc strings now parse deeply

--- server/api/parser/parser.controller.js
+++ server/api/parser/parser.controller.js
@@ -4,75 +4,138 @@ var _ = require('lodash');
 var Parser = require('./parser.model');
 var cache = {};
 
+var LogicDoc = {
+  parse: function (source, tags, parentTag) {
+    var result = source.replace(/\{\{(.*)\}\}/g, function (match, tag, offset, string) {
+      if (tags[tag]) {
+        if (typeof tags[tag] == 'function') {
+          return tags[tag]();
+        } 
+        else if (typeof tags[tag] == 'string') {
+          return LogicDoc.parse(tags[tag], tags, tag);
+        }
+        else
+          return tags[tag];
+      }
+      return "{{ NOMATCH: "+tag+" }}";
+    });
+
+    return result;
+  }
+}
+
+
 // Get list of parsers
 exports.index = function(req, res) {
 
   
 
 
-      var toml = require('toml'),
-          fs = require('fs');
+  var toml = require('toml'),
+      fs = require('fs');
 
-        var lraw = fs.readFileSync('./client/first.ldoc');
-        var ldoc = toml.parse(lraw);
+  var templateFile = './client/app/ldoc/ldoc.html';
 
-        if (ldoc.page) {
-          if (ldoc.page.lmod == 'angular-app') {
-            var templateFile = './client/app/ldoc/ldoc.html';
-            var mainJSPath = './client/components/logic/main.js';
-            var jsData = '';
+  // var lraw = fs.readFileSync('./client/first.ldoc');
+  var lraw = fs.readFileSync('./client/test.ldoc');
+  var ldoc = toml.parse(lraw);
 
-            if (ldoc.page.content && (cache["ldoc.page.content"] != ldoc.page.content)) {
-              cache["ldoc.page.content"] = ldoc.page.content;
-              fs.writeFileSync(templateFile, ldoc.page.content)
-            }
+  if (ldoc.page) {
+    if (ldoc.page.lmod == 'angular-app') {
+      var mainJSPath = './client/components/logic/main.js';
+      var jsData = '';
 
-            if (ldoc.page.js) {
-              // if (cache['ldoc.page.js'] != ldoc.page.js) {
-                // cache['ldoc.page.js'] = ldoc.page.js;
-                jsData += ldoc.page.js
-                // fs.writeFileSync(mainJSPath, ldoc.page.js);
-              // }
-            }
-            var keys = _.keys(ldoc);
+      if (ldoc.page.content && (cache["ldoc.page.content"] != ldoc.page.content)) {
+        cache["ldoc.page.content"] = ldoc.page.content;
+        fs.writeFileSync(templateFile, ldoc.page.content)
+      }
 
-            _.forEach(ldoc.page, function (item, index) {
-              var colon = index.split(':');
-              if (colon[0] == 'route' && colon[1]) {
-                var route = {};
-                route.state = 'ldoc.' + colon[1];
+      if (ldoc.page.js) {
+        // if (cache['ldoc.page.js'] != ldoc.page.js) {
+          // cache['ldoc.page.js'] = ldoc.page.js;
+          jsData += ldoc.page.js
+          // fs.writeFileSync(mainJSPath, ldoc.page.js);
+        // }
+      }
+      var keys = _.keys(ldoc);
 
-                if (item.template) {
-                  route.template = item.template;
-                }
-                if (item.url) {
-                  route.url = item.url;
-                }
+      _.forEach(ldoc.page, function (item, index) {
+        var colon = index.split(':');
+        if (colon[0] == 'route' && colon[1]) {
+          var route = {};
+          route.state = 'ldoc.' + colon[1];
 
-                var routeJs = fs.readFileSync('./client/templates/route/route.js').toString();
-
-                routeJs = routeJs.replace(/\{\{(.*)\}\}/g, function (match, tag, offset, string) {
-                  if (tag == 'root') {
-                    return JSON.stringify(route, true);
-                  } 
-                })
-
-                jsData += routeJs;
-              }
-            })
-
-            var fileData = fs.readFileSync(mainJSPath).toString();
-            if (fileData != jsData) {
-              console.log("fileData != jsData, setting new.\n\ncache:\n", fileData, '\n\njsData:\n', jsData, '\n---------');
-              fs.writeFileSync(mainJSPath, jsData);
-            }
-
-            res.sendfile('./client/index.html');
-            // res.json(200, ldoc);
+          if (item.template) {
+            route.template = item.template;
           }
-        }
+          if (item.url) {
+            route.url = item.url;
+          }
 
-        else res.json(200, ldoc);
+          var routeJs = fs.readFileSync('./client/templates/route/route.js').toString();
+
+          routeJs = routeJs.replace(/\{\{(.*)\}\}/g, function (match, tag, offset, string) {
+            if (tag == 'root') {
+              return JSON.stringify(route, true);
+            } 
+          });
+
+          jsData += routeJs;
+        }
+      })
+
+      var fileData = fs.readFileSync(mainJSPath).toString();
+      if (fileData != jsData) {
+        console.log("fileData != jsData, setting new.\n\ncache:\n", fileData, '\n\njsData:\n', jsData, '\n---------');
+        fs.writeFileSync(mainJSPath, jsData);
+      }
+
+      // res.json(200, ldoc);
+      res.sendfile('./client/index.html');
+    }
+  }
+  else if (ldoc.doc) {
+    var templateData = '',
+      docContent = '';
+
+    if (ldoc.settings) {
+      if (ldoc.settings.template) {
+        templateData = ldoc.settings.template;
+      }
+    }
+
+
+    _.forEach(ldoc.doc, function (item, key) {
+        var colon = key.split(':');
+        if (!colon[1]) {
+          if (typeof item == 'string') {
+            if (item.substr(0,3) == '!md') {
+              var marked = require('marked');
+              docContent += marked(item.replace(/^(!md\s*)/, ''));
+            } else
+              docContent += item;
+          } else {
+            docContent += JSON.stringify(item, true);
+          }
+        } else {
+          docContent += key + ": " + JSON.stringify(item, true);
+        }
+    })
+
+    if (templateData) {
+      var tags = _.merge({}, ldoc);
+      tags.content = docContent;
+
+      docContent = LogicDoc.parse(templateData, tags);
+
+      fs.writeFileSync(templateFile, docContent);
+    }
+
+      res.sendfile('./client/index.html');
+
+  }
+
+  else res.json(200, ldoc);
 
   // res.json(200, ldoc);
 
